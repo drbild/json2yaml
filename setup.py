d8f0963acee5a4b5fd6800754d8fd7a2ae5453f3
David R. Bild <david@davidbild.org>
2015-01-27 16:55:16 -0600

    add first version of conversion scripts

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+from os.path import join as pjoin
+
+setup(
+    name = 'json2yaml',
+    version = '1.0.0',
+    author = 'David R. Bild',
+    author_email = 'david@davidbild.org',
+    keywords = 'yaml json converter ordered order preserving',
+    url = 'https://github.com/drbild/json2yaml',
+    description = 'Convert JSON to YAML or vice versa, while'
+                  ' preserving the order of associative arrays.',
+    classifiers = [
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+    ],
+    scripts = [
+        pjoin('bin', 'json2yaml'),
+        pjoin('bin', 'yaml2json')
+    ],
+    install_requires = [
+        'pyyaml',
+        'pyaml',
+        'docopt'
+    ]
+)
