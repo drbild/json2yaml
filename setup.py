5f04baef5b6af78d7f880fa95d9a05ecfa796ff8
David R. Bild <david@davidbild.org>
2015-01-27 21:48:35 -0600

    increment version number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from os.path import join as pjoin
 
 setup(
     name = 'json2yaml',
-    version = '1.0.1',
+    version = '1.0.2-SNAPSHOT',
     author = 'David R. Bild',
     author_email = 'david@davidbild.org',
     keywords = 'yaml json converter ordered order preserving',
